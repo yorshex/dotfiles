37d6c54e249060896bd81d154aa8840e34303af0
yorshex <maxyorsh1324@gmail.com>
2023-12-18 10:11:08 +0500

    [dmenu] add patches

--- dmenu/config.h
+++ dmenu/config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 0;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Iosevka:size=12",
@@ -13,8 +14,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#ffffff", "#2b5d63" },
 	[SchemeOut] = { "#51a8b3", "#101012" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
+/* -l and -g options; controls number of lines and columns in grid if > 0 */
+static unsigned int lines      = 0;
+static unsigned int columns    = 0;
 
 /*
  * Characters not considered part of a word while deleting words
