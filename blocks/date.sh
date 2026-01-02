1978e68b126818a3c3d3622031bbe98b568c33d7
Maxim Ershov <maxyorsh1324@gmail.com>
2026-01-02 11:09:48 +0500

    fix `+` being called as a command rather than being a string in blocks/date.sh

--- blocks/date.sh
+++ blocks/date.sh
@@ -5,7 +5,7 @@ amon=`echo "($yday-1)/14" | bc`
 amday=`echo "($yday-1)%14" | bc`
 [ $yday -lt 365 ] &&
 	amonl=`echo "obase=16;65+$amon" | bc | xxd -r -p` ||
-	amonl=`+`
+	amonl='+'
 arv=`printf "%c%02d" $amonl $amday`
 
 date +"%a, %b %d %H:%M, $arv"
