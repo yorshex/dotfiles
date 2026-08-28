1bacae743f8c43835efcb153b004c74213e82ea7
Maxim Ershov <maxyorsh1324@gmail.com>
2026-08-28 20:20:34 +0500

    [bin] add brightness.sh script for controlling brightness

---
+++ bin/brightness.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+backlight_device='intel_backlight'
+
+devpath="/sys/class/backlight/${backlight_device}"
+brightpath="${devpath}/brightness"
+maxpath="${devpath}/max_brightness"
+
+error() {
+	echo "error: $1"
+	exit 1
+}
+
+domath() {
+	echo "$1" | bc || error "math error"
+}
+
+is_integer() {
+	case "${1#[+-]}" in
+	*[!0123456789]*) return 1 ;;
+	'') return 1 ;;
+	*) return 0 ;;
+	esac
+}
+
+# basic getters/setters
+get_max_brightness() {
+	[ -r "$maxpath" ] || error "no read permission"
+	cat "$maxpath"
+}
+
+get_brightness() {
+	[ -r "$brightpath" ] || error "no read permission"
+	cat "$brightpath"
+}
+
+set_brightness() {
+	[ -w "$brightpath" ] || error "no write permission"
+	echo "$1" > "$brightpath"
+}
+
+# 100-scale getters/setters
+get_brightness100() {
+	[ -z "$maxbright" ] && maxbright="$(get_max_brightness)"
+	domath "$(get_brightness) * 100 / ($maxbright - 1)"
+}
+
+set_brightness100() {
+	[ -z "$maxbright" ] && maxbright="$(get_max_brightness)"
+	local value="$1"
+	[ "$value" -lt 0 ] && local value=0
+	[ "$value" -gt 100 ] && local value=100
+	local rawvalue="$(domath "$value * ($maxbright - 1) / 100 + 1")"
+	[ "$rawvalue" -gt "$maxbright" ] && local rawvalue="$maxbright"
+	set_brightness "$rawvalue"
+}
+
+if is_integer "$1"; then
+	case "$1" in
+	[+-]*) value="$(domath "$(get_brightness100) $1")" ;;
+	*) value="$1" ;;
+	esac
+	set_brightness100 "$value"
+fi
+
+notify-send -e -u low "Brightness: $(get_brightness100)%"
