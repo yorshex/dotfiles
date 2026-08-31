a917f6a74f8271156e873d65fa8c77ed0541b652
Maxim Ershov <maxyorsh1324@gmail.com>
2026-08-31 11:22:05 +0500

    [blocks] fancy icons in battery.sh

--- blocks/battery.sh
+++ blocks/battery.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-level="$(cat /sys/class/power_supply/BAT0/capacity)"
+devpath=/sys/class/power_supply/BAT0
+level="$(cat "$devpath/capacity")"
+t="$(echo "($level+5)/10" | bc)"
+case "$t" in
+	0) i=󰂎 ;; 1) i=󰁺 ;; 2) i=󰁻 ;; 3) i=󰁼 ;;
+	4) i=󰁽 ;; 5) i=󰁾 ;; 6) i=󰁿 ;; 7) i=󰂀 ;;
+	8) i=󰂁 ;; 9) i=󰂂 ;; 10) i=󰁹 ;;
+esac
+[ "$(cat "$devpath/status")" = Charging ] && i="${i}󱐋"
 
-echo "󰁹 ${level}%"
+echo "${i} ${level}%"
