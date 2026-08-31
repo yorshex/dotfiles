6f644f0e1963b93a8a5bb7398345a1e7cb07eca6
Maxim Ershov <maxyorsh1324@gmail.com>
2026-08-31 10:34:56 +0500

    [bin/brightness.sh] tidy it a bit

--- bin/brightness.sh
+++ bin/brightness.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 
 backlight_device='intel_backlight'
-
 devpath="/sys/class/backlight/${backlight_device}"
-brightpath="${devpath}/brightness"
-maxpath="${devpath}/max_brightness"
 
-error() {
-	echo "error: $1"
-	exit 1
-}
+fnid=/tmp/yorshex_dotfiles-last_brightness_notification_id
+fbright="${devpath}/brightness"
+fmax="${devpath}/max_brightness"
 
-domath() {
-	echo "$1" | bc || error "math error"
-}
-
+error() { echo "error: $1"; exit 1; }
+domath() { echo "$1" | bc || error "math error"; }
+get_brightness() { domath "$(cat "$fbright") * 100 / $maxbright"; }
 is_digits() {
 	case "$1" in
 	*[!0123456789]*) return 1 ;;
@@ -23,56 +18,29 @@ is_digits() {
 	esac
 }
 
-is_integer() {
-	is_digits "${1#[+-]}"
-}
+[ -r "$fmax" ] || error "no read permission"
+[ -r "$fbright" ] || error "no read permission"
+[ -w "$fbright" ] || error "no write permission"
+maxbright="$(cat "$fmax")"
 
-# basic getters/setters
-get_max_brightness() {
-	[ -r "$maxpath" ] || error "no read permission"
-	cat "$maxpath"
-}
-
-get_brightness() {
-	[ -r "$brightpath" ] || error "no read permission"
-	cat "$brightpath"
-}
-
-set_brightness() {
-	[ -w "$brightpath" ] || error "no write permission"
-	echo "$1" > "$brightpath"
-}
-
-# 100-scale getters/setters
-get_brightness100() {
-	[ -z "$maxbright" ] && maxbright="$(get_max_brightness)"
-	domath "$(get_brightness) * 100 / ($maxbright - 1)"
-}
-
-set_brightness100() {
-	[ -z "$maxbright" ] && maxbright="$(get_max_brightness)"
-	local value="$1"
-	[ "$value" -lt 0 ] && local value=0
-	[ "$value" -gt 100 ] && local value=100
-	local rawvalue="$(domath "$value * ($maxbright - 1) / 100 + 1")"
-	[ "$rawvalue" -gt "$maxbright" ] && local rawvalue="$maxbright"
-	set_brightness "$rawvalue"
-}
-
-if is_integer "$1"; then
+if is_digits "${1#[+-]}"; then
 	case "$1" in
-	[+-]*) value="$(domath "$(get_brightness100) $1")" ;;
+	[+-]*) value="$(domath "$(get_brightness) $1")" ;;
 	*) value="$1" ;;
 	esac
-	set_brightness100 "$value"
+
+	[ "$value" -lt 0 ] && value=0
+	[ "$value" -gt 100 ] && value=100
+	rawvalue="$(domath "$value * $maxbright / 100 + 1")"
+	[ "$rawvalue" -gt "$maxbright" ] && rawvalue="$maxbright"
+	[ "$rawvalue" -lt 1 ] && rawvalue=1
+	echo "$rawvalue" > "$fbright"
 fi
 
 if [ "$DISPLAY" ]; then
-	nidfile=/tmp/yorshex_dotfiles-last_brightness_notification_id
-	nid=0
-	[ -f "$nidfile" ] && nid="$(cat "$nidfile")"
-	is_digits "$nid" || nid=0
-	notify-send -u low -e -r "$nid" -p "Brightness: $(get_brightness100)%" >"$nidfile"
+	nid="$(cat "$fnid")"
+	is_digits "$nid" && nidarg="-r $nid"
+	notify-send -u low -e $nidarg -p "Brightness: $(get_brightness)%" >"$fnid"
 else
-	echo "Brightness: $(get_brightness100)%"
+	echo "Brightness: $(get_brightness)%"
 fi
